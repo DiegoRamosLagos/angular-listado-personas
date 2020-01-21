11116cdf20320fc0d6f0810f3cb606abe1812c6f
dramos <diego.ramos@imagemaker.com>
2020-01-21 17:36:48 -0300

    Agregamos un servicio dentro de otro servicio por medio de @Injectable

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -1,12 +1,18 @@
+import { LoggingService } from './LoggingService.service';
 import { Persona } from './persona.model';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class PersonasService {
     personas: Persona[] = [
         new Persona('Juan', 'Perez'),
         new Persona('Laura', 'Juarez')
     ];
 
+    constructor(private loggingService: LoggingService) {}
+
     agregarPersona(persona: Persona) {
+        this.loggingService.enviaMensajeAConsola(`agregamos persona ${persona.nombre}`);
         this.personas.push(persona);
       }
 }
