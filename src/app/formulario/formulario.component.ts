365359f757aacb775d43f4260f86952a8932c0bc
dramos <diego.ramos@imagemaker.com>
2020-01-22 11:54:58 -0300

    Mejorando el estilo de la aplicación

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -11,21 +11,20 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 export class FormularioComponent implements OnInit {
   // Vamos a enviar información al componente padre
   // @Output() personaCreada = new EventEmitter<Persona>(); // Debe ser de @angular/core
-  // nombreInput: string;
-  // apellidoInput: string;
-
-  @ViewChild('nombreInput') nombreInput: ElementRef;
-  @ViewChild('apellidoInput') apellidoInput: ElementRef;
+  nombreInput: string;
+  apellidoInput: string;
 
   constructor(private loggingService: LoggingService,
-              private personasService: PersonasService) { }
+              private personasService: PersonasService) {
+                this.personasService.saludar.subscribe((indice: number) => alert(`El índice es ${indice}`))
+              }
 
   ngOnInit() {
   }
 
   onAgregarPersona(){
     // Estamos usando referencia local con viewChild
-    const persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
+    const persona1 = new Persona(this.nombreInput, this.apellidoInput);
     // Para agregar la persona debemos informárselo a nuestra clase padre App component
     // this.loggingService.enviaMensajeAConsola(`enviamos persona ${persona1.nombre} ${persona1.apellido}`);
     // this.personaCreada.emit(persona1);
